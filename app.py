8dce93f90909503b866acb58034103adaf90e981
Saurabh Native <codeclassifiers@gmail.com>
2020-07-26 13:10:21 +0530

    added cors

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, make_response, request, abort
 import pandas as pd
 import catboost
 import pickle
+from flask_cors import CORS,cross_origin
 model = pickle.load( open( "finalized_model.sav", "rb" ) )
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+cors = CORS(app)
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
@@ -13,11 +16,14 @@ def not_found(error):
 def hello():
   return "Hello World!"
 
-@app.route("/get_prediction", methods=['POST'])
+@app.route("/get_prediction", methods=['POST','OPTIONS'])
+@cross_origin()
 def create_task():
     if not request.json:
         abort(400)
-    df = pd.DataFrame(request.json, index=[0])  
+    df = pd.DataFrame(request.json, index=[0])
+    cols=["CONSOLE","RATING","CRITICS_POINTS","CATEGORY","YEAR","PUBLISHER","USER_POINTS"]
+    df = df[cols]
     return jsonify({'result': model.predict(df)[0]}), 201
 
 if __name__ == "__main__":
